3f65b6872e2aa69ffa5e64ae21b6406161d7e047
ewy116554 <44164793+Elia16@users.noreply.github.com>
2022-06-23 09:37:00 +0200

    added new Controller

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -5,10 +5,25 @@ namespace M184\Controller;
  */
 class DefaultController
 {
-
+  $db;
   function __construct()
   {
-    // code...
+    $this->db = new \mysql();
+      if($this->valid_token() === true){
+        header("Location: /home");
+        exit();
+      }else {
+        header("Location: /login");
+        exit();
+      }
+  }
+  function valid_token(){
+    if(isset($_SESSION['token'])){
+
+    }else{
+      return false;
+    }
+
   }
 }
 
