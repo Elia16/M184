e85471978deafbfc3a58d7922d6137c18f2667aa
DracoAlatus <66775322+DracoAlatus@users.noreply.github.com>
2022-06-23 09:01:33 +0200

    Added User Table creation script

---
+++ User_table_script.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS usersDb;
+
+USE usersDb;
+
+        CREATE TABLE users(
+            id VARCHAR(255) auto_increment NOT NULL,
+            username VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            last_login TIMESTAMP);
+            
